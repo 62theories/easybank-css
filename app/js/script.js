bd9ecba0d7067c8954bd39b7d0e022b6e2d5d29d
62theories <s5935512005@phuket.psu.ac.th>
2021-10-04 11:15:44 +0700

    เมนูเสร็จแล้ว

--- app/js/script.js
+++ app/js/script.js
@@ -1,17 +1,25 @@
 const btnHamburger = document.querySelector("#btnHamburger");
 const header = document.querySelector(".header");
 const overlay = document.querySelector(".overlay");
+const fadeElems = document.querySelectorAll(".has-fade");
+const body = document.querySelector("body");
 
 btnHamburger.addEventListener("click", function () {
   if (header.classList.contains("open")) {
-    // close munu
+    // close menu
+    body.classList.remove("no-scroll");
     header.classList.remove("open");
-    overlay.classList.remove("fade-in");
-    overlay.classList.add("fade-out");
+    fadeElems.forEach(function (element) {
+      element.classList.remove("fade-in");
+      element.classList.add("fade-out");
+    });
   } else {
     // open menu
+    body.classList.add("no-scroll");
     header.classList.add("open");
-    overlay.classList.remove("fade-out");
-    overlay.classList.add("fade-in");
+    fadeElems.forEach(function (element) {
+      element.classList.remove("fade-out");
+      element.classList.add("fade-in");
+    });
   }
 });
